2062ea23239681f28802605b903a5313abb7afdf
Lukasz Magiera <lukaszmagiera26@gmail.com>
2018-09-18 12:13:44 +0200

    U

---
+++ Magic_Number/MagicNumber_v2/src/CheckPNG.java
@@ -0,0 +1,16 @@
+
+
+public class CheckPNG {
+
+    private final int PNG_MAGIC[] = new int[]{0x89, 0x50, 0x4e, 0x47, 0x0d, 0x0a, 0x1a, 0x0a};
+
+    boolean isPng(int[] bytes)  {
+        boolean result = true;
+        for (int i = 0; i < PNG_MAGIC.length; ++i) {
+            if (bytes[i] != PNG_MAGIC[i]) {
+                result = false;
+            }
+        }
+        return result;
+    }
+}
